b5493556ca16b62efaad89d16f7ac702de73b923
Vaibhav Gupta <vaibhgupt199@gmail.com>
2018-04-05 21:03:22 +0530

    Setup For Static Files

--- VHBooking_Portal/settings.py
+++ VHBooking_Portal/settings.py
@@ -119,6 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static_my_proj'),
+]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn', 'static_root')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn', 'media_root')
+
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'homepage'
 LOGOUT_REDIRECT_URL = 'login'
