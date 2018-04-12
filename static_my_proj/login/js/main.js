d224bc2fb44083b7c593492af3766d9d75c179bf
Vaibhav Gupta <vaibhgupt199@gmail.com>
2018-04-12 19:30:39 +0530

    Theme Change For Login Page

--- static_my_proj/login/js/main.js
+++ static_my_proj/login/js/main.js
@@ -2,7 +2,21 @@
 (function ($) {
     "use strict";
 
-    
+
+    /*==================================================================
+    [ Focus input ]*/
+    $('.input100').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+  
+  
     /*==================================================================
     [ Validate ]*/
     var input = $('.validate-input .input100');
@@ -52,6 +66,24 @@
         $(thisAlert).removeClass('alert-validate');
     }
     
-    
+    /*==================================================================
+    [ Show pass ]*/
+    var showPass = 0;
+    $('.btn-show-pass').on('click', function(){
+        if(showPass == 0) {
+            $(this).next('input').attr('type','text');
+            $(this).find('i').removeClass('zmdi-eye');
+            $(this).find('i').addClass('zmdi-eye-off');
+            showPass = 1;
+        }
+        else {
+            $(this).next('input').attr('type','password');
+            $(this).find('i').addClass('zmdi-eye');
+            $(this).find('i').removeClass('zmdi-eye-off');
+            showPass = 0;
+        }
+        
+    });
+
 
 })(jQuery);
